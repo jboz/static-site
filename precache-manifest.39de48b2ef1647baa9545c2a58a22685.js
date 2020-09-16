84f9c2d25b1297846bee5100f91e5a49cf41a147
Julien Boz <julienboz@gmail.com>
2020-09-16 17:52:49 +0200

    Updates

---
+++ precache-manifest.39de48b2ef1647baa9545c2a58a22685.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55696d7c4fc9032e327061222ce7ef51",
+    "url": "/static-site/index.html"
+  },
+  {
+    "revision": "8172c203bbab5907a359",
+    "url": "/static-site/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fe21655dc53a977d9150",
+    "url": "/static-site/static/js/2.3952cb03.chunk.js"
+  },
+  {
+    "revision": "54fd6816419f7824e2804745930c9117",
+    "url": "/static-site/static/js/2.3952cb03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8172c203bbab5907a359",
+    "url": "/static-site/static/js/main.5a001e0a.chunk.js"
+  },
+  {
+    "revision": "9b7c281ee2156ff44b17",
+    "url": "/static-site/static/js/runtime-main.5e2b605a.js"
+  }
+]);
